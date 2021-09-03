012d23790c45070792fb3704ab18045e2662dd7d
otienonick <nick.odongi@student.moringaschool.com>
2021-09-03 09:01:50 +0300

    class User and a function that will allow me to create new instances of the class

---
+++ user.py
@@ -0,0 +1,19 @@
+class User:
+    
+    '''
+    a class that generates an instance of a new user
+
+    '''
+    user_list =[]
+    
+    def __init__(self,username,password):
+
+        '''
+        a method that defines the properties of the class object
+
+        '''
+        
+        self.username =username
+        self.password = password
+        
+ 
