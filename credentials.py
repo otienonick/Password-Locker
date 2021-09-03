dec231c1e6f29a5abe5e2a811cfeff2f50ad127e
otienonick <nick.odongi@student.moringaschool.com>
2021-09-03 20:03:35 +0300

    added the innit method to allow the creation of instance variables

--- credentials.py
+++ credentials.py
@@ -1,3 +1,25 @@
 class Credentials:
     
+    '''
+    class that generates a new instance of credentials.
+    '''
+    
     credentials_list = []
+
+    def __init__(self,username,password):
+
+        '''
+        a method that defines the properties of the class object.
+
+        '''
+        
+        self.username = username
+        self.password = password
+
+    def save_credentials(self):
+
+        '''
+         method that saves  credentials.
+            
+        '''
+        Credentials.credentials_list.append(self)    
