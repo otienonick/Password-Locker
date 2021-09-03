3fea1fb70cf2754017704ce0f8330c0caa788304
otienonick <nick.odongi@student.moringaschool.com>
2021-09-03 22:43:06 +0300

    added the find users,user exists and display user credentials test cases

--- test_credentials.py
+++ test_credentials.py
@@ -33,5 +33,54 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(len(Credentials.credentials_list),1)
 
 
+    def tearDown(self):
+
+        '''
+        teardown method that does clean up after each test case has run.
+
+        '''
+        Credentials.credentials_list = []
+        
+    def test_save_multiple_credentials(self):
+        
+        '''
+        testcase to check if user can save multiple credentials to the credentials_list
+
+        '''
+        self.new_credential.save_credentials()
+        test_credentials = Credentials('app','credential','2021')
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)  
+
+    def test_find_by_app(self):
+        
+        '''
+        test to check if we can find a user's credentials by appname and display information.
+
+        '''
+
+        self.new_credential.save_credentials()
+        test_credentials = Credentials('app','credential','2021') # new contact
+        test_credentials.save_credentials()
+
+        found_credential = Credentials.find_by_app('app')
+
+        self.assertEqual(found_credential.username,test_credentials.username)
+
+    def test_credential_exists(self):
+
+        '''
+        test to check if we can return a Boolean  if we cannot find the user's credentials.
+
+        '''
+        self.new_credential.save_credentials()
+        test_credentials = Credentials('app','credential','2021') # new contact
+        test_credentials.save_credentials()
+
+        credential_exists = Credentials.user_credential_exist('app')
+
+        self.assertTrue(credential_exists)
+
+
 if __name__ == '__main__':
     unittest.main()
