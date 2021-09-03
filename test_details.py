ddd86f2e3a0946ae46823225c311dbbb87922329
otienonick <nick.odongi@student.moringaschool.com>
2021-09-03 14:10:18 +0300

    added the save user testcase

--- test_details.py
+++ test_details.py
@@ -16,7 +16,18 @@ class TestUser(unittest.TestCase):
         '''
         testcase to check if object or class is instanciated correctly
 
-        '''    
+        '''
+        self.assertEqual(self.new_user.username,'nick') 
+        self.assertEqual(self.new_user.password,'1982') 
+
+    def test_save_user(self):
+
+        '''
+        testcase to check if user details have been saved.
+
+        '''  
+        self.new_user.save_user() #saving new user
+        self.assertEqual(len(User.user_list),1)  
 
 
         
