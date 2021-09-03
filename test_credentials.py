fcebf2227a69090739676a13699cd2897af11894
otienonick <nick.odongi@student.moringaschool.com>
2021-09-03 21:03:21 +0300

    added save existing user credentials testcase

--- test_credentials.py
+++ test_credentials.py
@@ -10,7 +10,7 @@ class TestCredentials(unittest.TestCase):
         a method that runs before any test
 
         '''
-        self.new_credential  = Credentials('oti','0084') #create a dummy credential
+        self.new_credential  = Credentials('twitter','oti','0084') #create a dummy credential
 
     def test_init(self):
         
@@ -18,11 +18,19 @@ class TestCredentials(unittest.TestCase):
         testcase to check if object or class is instanciated correctly
 
         '''
+        self.assertEqual(self.new_credential.app_name,'twitter') 
         self.assertEqual(self.new_credential.username,'oti') 
         self.assertEqual(self.new_credential.password,'0084') 
 
 
-    
+    def test_save_credentials(self):
+
+        '''
+        testcase method to save objects into credentials list.
+
+        '''
+        self.new_credential.save_credentials() #saving credential
+        self.assertEqual(len(Credentials.credentials_list),1)
 
 
 if __name__ == '__main__':
